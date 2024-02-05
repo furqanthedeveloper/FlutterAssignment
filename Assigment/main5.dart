797bcf672d832960e3c3590c795cb53788258092
Muhammad Furqan <155351309+furqanthedeveloper@users.noreply.github.com>
2024-02-05 19:10:02 +0500

    Add files via upload

---
+++ Assigment/main5.dart
@@ -0,0 +1,11 @@
+void main() {
+ 
+ 
+ 
+ 
+}
+ //Map <dynamic,dynamic> lc_name ={"123345667" : "Taha",
+ //                               "1234" : "Ghazanfar",
+ //                               "232321" : "Mustafa",
+ //                               "3421" : "Asad Bhai"};
+// print(lc_name.keys.where((e) => e.length == 4)); 
